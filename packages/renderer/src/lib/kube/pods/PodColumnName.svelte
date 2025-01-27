815ae8d36566d90ad01a976b925c09543e601923
Tim deBoer <git@tdeboer.ca>
2025-01-27 14:18:17 -0500

    feat: kube pods page
    Adds a Kubernetes Pods page, similar to Deployments/Services/etc, but with the
    same columns and actions as the Kubernetes pods in the current Pods page.
    
    Given the number of files just to add the page, this commit intentionally does not
    include a details page. That would come in following PRs.
    
    Since we already have a PodsList in src/lib/pod, I've put this in /src/lib/kube/pods,
    not sure if there is a better location or we should start organizing Kube folders
    differently.
    
    Part of #8818.
    
    Signed-off-by: Tim deBoer <git@tdeboer.ca>

---
+++ packages/renderer/src/lib/kube/pods/PodColumnName.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+import type { PodUI } from './PodUI';
+
+export let object: PodUI;
+</script>
+
+<div class="flex flex-col max-w-full">
+  <div class="text-[var(--pd-table-body-text-highlight)] max-w-full overflow-hidden text-ellipsis">
+    {object.name}
+  </div>
+  <div class="flex flex-row text-sm gap-1">
+    {#if object.namespace}
+      <div class="font-extra-light text-[var(--pd-table-body-text)]">{object.namespace}</div>
+    {/if}
+  </div>
+</div>
